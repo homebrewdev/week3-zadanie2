c21d74f744863941ca6bba16194cf2bd8daec275
homebrewdev <xstall@yandex.ru>
2019-06-24 23:10:15 +0400

    Initial Commit

---
+++ week3-zadanie2/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  week3-zadanie2
+//
+//  Created by tiger on 24.06.2019.
+//  Copyright © 2019 homework. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
